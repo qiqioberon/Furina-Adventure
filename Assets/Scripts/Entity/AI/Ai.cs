c0e6985991828a6c809ffc40b68706395c95f578
Yutsss <adyuta123@gmail.com>
2025-05-26 13:43:25 +0700

    fix: change a* to bfs 2

--- Assets/Scripts/Entity/AI/Ai.cs
+++ Assets/Scripts/Entity/AI/Ai.cs
@@ -2,23 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Actor), typeof(AStar))]
+[RequireComponent(typeof(Actor), typeof(BFSPathfinder))]
 public class Ai : MonoBehaviour
 {
-    [SerializeField] private AStar aStar;
+    [SerializeField] private BFSPathfinder pathfinder;
+    public BFSPathfinder Pathfinder { get => pathfinder; set => pathfinder = value; }
 
-    public AStar AStar { get => aStar; set => aStar = value; }
+    private void OnValidate() => pathfinder = GetComponent<BFSPathfinder>();
 
-    public void OnValidate() => aStar = GetComponent<AStar>();
-
-    public void MoveAlongPath(Vector3Int targetPos, float movementSpeed){
-        // Debug.Log("Moving along path");
-        Vector3Int gridPos = MapManager.Instance.FloorMap.WorldToCell(transform.position); 
-        Vector2 dir = aStar.Compute((Vector2Int)gridPos, (Vector2Int)targetPos) * movementSpeed * Time.deltaTime;
+    public void MoveAlongPath(Vector3Int targetPos, float movementSpeed) {
+        Vector3Int gridPos = MapManager.Instance.FloorMap.WorldToCell(transform.position);
+        Vector2 dir = pathfinder.Compute((Vector2Int)gridPos, (Vector2Int)targetPos)
+                      * movementSpeed * Time.deltaTime;
         Action.MovementAction(GetComponent<Actor>(), dir);
     }
+
     public virtual AiState SaveState() => new AiState();
 }
+
+
 
 [System.Serializable]
 public class AiState {
--- Assets/Scripts/Entity/AI/Types/HostileEnemy.cs
+++ Assets/Scripts/Entity/AI/Types/HostileEnemy.cs
@@ -8,10 +8,7 @@ public class HostileEnemy : Ai
     [SerializeField] protected Fighter fighter;
     [SerializeField] protected bool isFighting;
     [SerializeField] protected bool canTakeDamage = false;
-
-
-    // [SerializeField] private float movementCooldown = 0.5f; // Adjust as needed
-    [SerializeField] protected float attackCooldown = 0.1f; // Adjust as needed
+    [SerializeField] protected float attackCooldown = 0.1f;
     [SerializeField] protected int expGiven;
     [SerializeField] protected int moraGiven;
     [SerializeField] protected float attackTimer = 0.1f;
@@ -20,48 +17,37 @@ public class HostileEnemy : Ai
     public bool IsFighting { get => isFighting; set => isFighting = value; }
     public bool CanTakeDamage { get => canTakeDamage; set => canTakeDamage = value; }
     public int MoraGiven { get => moraGiven; set => moraGiven = value; }
-    // private float movementTimer = 0.5f;
-    private void OnValidateOverride(){
+
+    private void OnValidateOverride() {
         fighter = GetComponent<Fighter>();
-        AStar = GetComponent<AStar>();
+        Pathfinder = GetComponent<BFSPathfinder>();
     }
 
-
-    public virtual void RunAI(){
-        
-        if(!fighter.Target){
+    public virtual void RunAI() {
+        if (!fighter.Target) {
             fighter.Target = GameManager.Instance.Actors[0];
         }
-        else if(fighter.Target && !fighter.Target.IsAlive){
+        else if (fighter.Target && !fighter.Target.IsAlive) {
             fighter.Target = null;
         }
 
-        if(fighter.Target && fighter.Target.IsAlive){
+        if (fighter.Target && fighter.Target.IsAlive) {
             Vector3Int targetPos = MapManager.Instance.FloorMap.WorldToCell(fighter.Target.transform.position);
-            // isFighting = true;
-            if(isFighting || GetComponent<Actor>().FieldOfView.Contains(targetPos)){
-                // Debug.Log("Fighting");
-                if(!isFighting){
-                    isFighting = true;
-                }
 
-                float targetDistance = Vector3.Distance(transform.position, fighter.Target.transform.position);
+            if (isFighting || GetComponent<Actor>().FieldOfView.Contains(targetPos)) {
+                if (!isFighting) isFighting = true;
 
-                if(targetDistance <= 1f){
-                    if(attackTimer > attackCooldown){
+                float dist = Vector3.Distance(transform.position, fighter.Target.transform.position);
+
+                if (dist <= 1f) {
+                    if (attackTimer > attackCooldown) {
                         Action.MeleeAction(GetComponent<Actor>(), fighter.Target);
                         attackTimer = 0;
                         return;
                     }
                 }
-                else{
-                    
-                    // if(movementTimer > 0){
-                    //     // Debug.Log(movementTimer);
-                    //     return;
-                    // }
-                    // movementTimer = movementCooldown;
-                    MoveAlongPath(targetPos, GetComponent<Fighter>().MovementSpeed);
+                else {
+                    MoveAlongPath(targetPos, fighter.MovementSpeed);
                     return;
                 }
             }
